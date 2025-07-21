e4bf7d2512192e794641bb472d6ca9d19fb29efe
j-toutatis <jack.benmiller1@protonmail.com>
2025-07-21 14:57:38 -0700

    added count of expired pswds. also fixed logic for warnings

--- Password_Monitor.ps1
+++ Password_Monitor.ps1
@@ -3,16 +3,22 @@ Connect-MgGraph -Scopes "user.read.all" -NoWelcome
 
 $users = Get-MgUser -All -Property DisplayName, UserPrincipalName, LastPasswordChangeDateTime
 $current_Date = Get-Date
+$total_Expired = 0
+$total_Warning = 0
 foreach ($user in $users){
     $expiry_Date = $($user.LastPasswordChangeDateTime).AddDays(270)
     $warning_Date = $($user.LastPasswordChangeDateTime).AddDays(265)
     if ($expiry_Date -le $current_Date){
         Write-Host "the password for user: '$($user.DisplayName)' has expired. It was set on '$($user.LastPasswordChangeDateTime)'. Email the user at '$($user.UserPrincipalName)' to reset their password."
+        $total_Expired++
     }
-    elseif ($expiry_Date -le $warning_Date){
-        Write-Host "the password for user: '$($user.DisplayName)' will expire on '$warning_Date'"
+    elseif ($warning_Date -le $current_Date){
+        Write-Host "the password for user: '$($user.DisplayName)' will expire on '$expiry_Date'"
+        $total_Warning++
     }
     else {
         continue
     }
 }
+Write-Host "Total expired passwords: $total_Expired"
+Write-Host "Total passwords approaching expiry: $total_Warning"
