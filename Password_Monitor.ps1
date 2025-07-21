d30b7671888c538ba17052d0be241696df7f053b
j-toutatis <jack.benmiller1@protonmail.com>
2025-07-21 09:28:50 -0700

    added warning for users approaching expiry

--- Password_Monitor.ps1
+++ Password_Monitor.ps1
@@ -1,12 +1,16 @@
-Connect-MgGraph -Scopes "user.read.all"
+Connect-MgGraph -Scopes "user.read.all" -NoWelcome
 
 
 $users = Get-MgUser -All -Property DisplayName, UserPrincipalName, LastPasswordChangeDateTime
 $current_Date = Get-Date
 foreach ($user in $users){
     $expiry_Date = $($user.LastPasswordChangeDateTime).AddDays(270)
+    $warning_Date = $($user.LastPasswordChangeDateTime).AddDays(265)
     if ($expiry_Date -le $current_Date){
-        Write-Host "the password for user: '$($user.DisplayName)' has expired. It was set on '$($user.LastPasswordChangeDateTime)'"
+        Write-Host "the password for user: '$($user.DisplayName)' has expired. It was set on '$($user.LastPasswordChangeDateTime)'. Email the user at '$($user.UserPrincipalName)' to reset their password."
+    }
+    elseif ($expiry_Date -le $warning_Date){
+        Write-Host "the password for user: '$($user.DisplayName)' will expire on '$warning_Date'"
     }
     else {
         continue
